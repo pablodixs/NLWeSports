62521f1c65d15011851d34f96b774e3949611608
Pablo Dias <pabllosoarez@gmail.com>
2022-09-13 02:15:47 -0300

    finished frontpage layout

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,5 @@
 import './styles/main.css'
+import { MagnifyingGlassPlus } from 'phosphor-react'
 import logoImage from './assets/logo-nlw.svg'
 
 export default function App() {
@@ -8,24 +9,61 @@ export default function App() {
       <h1 className='text-6xl text-white font-black mt-20'>Seu <span className='text-transparent bg-nlw-gradient bg-clip-text'>duo</span> está aqui</h1>
 
       <div className='grid grid-cols-6 gap-6 mt-16'>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game1.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>League of Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game2.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Dota 2</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game3.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>CS:G0</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game4.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Apex Legends</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game5.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>Fortnite</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
-        <a href="">
+        <a href="" className='relative rounded-lg overflow-hidden'>
           <img src="/game6.png" />
+          <div className='w-full pt-16 pb-4 px-4 bg-game-gradient absolute bottom-0 left-0 right-0'>
+            <strong className='font-bold text-white block'>World of Warcraft</strong>
+            <span className='text-zinc-300 text-sm block mt-1'>4 anúncios</span>
+          </div>
         </a>
+      </div>
+
+      <div className='pt-2 self-stretch bg-card-gradient mt-8 rounded-lg overflow-hidden'>
+        <div className='bg-[#2A2634] px-8 py-6 rounded-lg flex justify-between items-center'>
+          <div>
+            <h2 className='text-white text-2xl font-bold'>Não encontrou seu duo?</h2>
+            <p className='text-zinc-400'>Publique um anúncio para encontrar novos players!</p>
+          </div>
+          <button className='py-3 px-4 bg-violet-500 text-white rounded hover:bg-violet-600 flex items-center gap-3'>
+            <MagnifyingGlassPlus size={24} />
+            Publicar anúncio
+          </button>
+        </div>
       </div>
     </div>
   )
--- web/tailwind.config.cjs
+++ web/tailwind.config.cjs
@@ -9,7 +9,12 @@ module.exports = {
       backgroundImage: {
         'nlw-gradient': 'linear-gradient(89.86deg, #9572FC 23.08%, #43E7AD 33.94%, #E1D55D 33.57%)',
         background: "url('public/background.png')",
+        'game-gradient': 'linear-gradient(180deg, rgba(0, 0, 0, 0) 0%, rgba(0, 0, 0, 0.9) 67.08%);',
+        'card-gradient': 'linear-gradient(90deg, #9572FC 0%, #43E7AD 50.52%, #E2D45C 100%);',        
       },
+      fontFamily: {
+        sans: ['Inter', 'sans-serif']
+      }
     },
   },
   plugins: [],
